2664b6ec351b6c4561bc344082a1cd95d7801b5c
gustavo <gustavomonteiro7865@gmail.com>
2025-11-04 19:36:54 -0300

    alteracao na variavel senha

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -6,11 +6,13 @@ export class AuthController {
 
     public login = async (req: Request, res: Response) => {
         try {
-            const { email, password } = req.body;
-            const result = await this.authBusiness.login({ email, senha });
+            const { email, senha } = req.body; 
+
+            const result = await this.authBusiness.login({ email, senha }); 
+            
             res.status(200).send(result);
         } catch (error: any) {
             res.status(400).send({ error: error.message });
-        }
-    };
+        }
+    };
 }
