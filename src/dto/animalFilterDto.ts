ba4e4905f65002e1068e15960bf201650f28fd8b
Joyce <joycemasallajorge2006@gmail.com>
2025-11-11 21:42:28 -0300

    Adição do type do animl no DTO

--- src/dto/animalFilterDto.ts
+++ src/dto/animalFilterDto.ts
@@ -8,3 +8,13 @@ export interface AnimalFilterDTO extends PaginationParams {
     sortBy?: 'id_animal' | 'nome' | 'data_registro' | 'status'; 
     sortOrder?: 'asc' | 'desc'; 
 }
+
+// // atributos necessários para criar um Animal no banco sem o id_animal e o data que é gerada automaticamente
+export type AnimalInputForDB = {
+    nome: string;
+    especie: string;
+    descricao: string;
+    status: string;
+    localizacao: string;
+    ong_id: number;
+}
